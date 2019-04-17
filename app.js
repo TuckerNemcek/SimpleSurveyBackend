46b51f00ef84c6394678fccd22173218fcd10358
Tucker Nemcek <tuckernemcek21@gmail.com>
2019-04-17 14:24:48 -0600

    well that was a dissapointment

--- app.js
+++ app.js
@@ -10,13 +10,13 @@ var cors = require('cors')
 
 
 
-app.get('/products/:id', function (req, res, next) {
-  res.json({msg: 'This is CORS-enabled for all origins!'})
-})
-
-app.listen(80, function () {
-  console.log('CORS-enabled web server listening on port 80')
-})
+// app.get('/products/:id', function (req, res, next) {
+//   res.json({msg: 'This is CORS-enabled for all origins!'})
+// })
+//
+// app.listen(80, function () {
+//   console.log('CORS-enabled web server listening on port 80')
+// })
 
 
 app.use(function(req, res, next) {
